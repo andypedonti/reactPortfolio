10b7520beaed219fcb2f09d9b7ef5456dd010e2a
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-11 14:19:26 -0400

    the buttons have a background

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,26 @@
 import React from "react";
+import "./style.css";
 
 function NavBar() {
     return (
-        <div>
-            <button>about</button>
-            <button>portfolio</button>
-            <button>contact</button>
-        </div>
+
+        <wrapper >
+
+
+
+            <h1 className="andy">Andy Pedonti </h1>
+            <div className="buttonBar">
+
+                <div className="buttons">
+                    <button>about</button>
+                    <button>portfolio</button>
+                    <button>contact</button>
+                </div>
+            </div>
+
+        </wrapper>
+
+
     )
 }
 
