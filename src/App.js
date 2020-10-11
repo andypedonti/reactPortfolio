8b28c4c5a0ad07f062b8b8416425834c34417476
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-11 14:43:32 -0400

    imported router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 import NavBar from './components/Navbar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 class App extends React.Component {
@@ -14,7 +15,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <NavBar />
+      <Router>
+        <NavBar />
+      </Router>
     );
   }
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function NavBar() {
     return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/about"
+                            }
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/portfolio"
+                            className={
+                                window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+                        >
 
-        <wrapper >
+                            Portfolio
+                        </Link>
 
-
-
-            <h1 className="andy">Andy Pedonti </h1>
-            <div className="buttonBar">
-
-                <div className="buttons">
-                    <button>about</button>
-                    <button>portfolio</button>
-                    <button>contact</button>
-                </div>
+                    </li>
+                </ul>
             </div>
+        </nav>
 
-        </wrapper>
+
 
 
     )
