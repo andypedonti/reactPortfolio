8c350f194463c11a5a535014c179e7da164e927f
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-15 18:48:00 -0400

    about pic works

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Card from "../components/Card";
+import "./style.css";
 
 
 function About() {
     return (
         <Card>
             <div class="card-body">
-                <img class="card-img-top"></img>
+                <img src="./components/pics/018.jpg" height="225" width="150" >
+
+
+                </img>
                 <h4 class="card-title">About Me</h4>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Husband, father, angler</h6>
 
             </div>
         </Card>
