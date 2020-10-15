2ec2ba28b845a50a606fb1672a66814766ba9ae0
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-15 16:35:22 -0400

    portfolio works

--- src/pages/portfolioContent.js
+++ src/pages/portfolioContent.js
@@ -1,30 +1,58 @@
 export const portfolioContent = [
     {
-        title: "Project Name",
-
-
-        url: "https://via.placeholder.com/150"
-
+        title: "codeRefractor",
+        url: "https://andypedonti.github.io/HomeWorkWk1/"
     },
     {
-        title: "Project Name",
-
-
-        url: "https://via.placeholder.com/150"
-
+        title: "Bootstrap CSS",
+        url: "https://andypedonti.github.io/HomeWorkWk2/"
     },
     {
-        title: "Project Name",
-
-
-        url: "https://via.placeholder.com/150"
-
+        title: "passwordGenerator",
+        url: "https://andypedonti.github.io/passWord/"
     },
     {
-        title: "Project Name",
-
-
-        url: "https://via.placeholder.com/150"
-
-    }
+        title: "javaScriptQuiz",
+        url: "https://andypedonti.github.io/javaQuiz/"
+    },
+    {
+        title: "dayPlanner",
+        url: "https://andypedonti.github.io/workDayPlanner/"
+    },
+    {
+        title: "weatherApp",
+        url: "https://andypedonti.github.io/weatherApp/"
+    },
+    {
+        title: "projectOne",
+        url: "https://andypedonti.github.io/Party-Planning-Committee/"
+    },
+    {
+        title: "readmeGenerator",
+        url: "https://github.com/andypedonti/pesticideRecord"
+    },
+    {
+        title: "teamProfileGenerator",
+        url: "https://github.com/andypedonti/templateEngine"
+    },
+    {
+        title: "projectTwo",
+        url: "https://lit-cove-09290.herokuapp.com/"
+    },
+    {
+        title: "updatedPortfolio2",
+        url: "https://andypedonti.github.io/updatedPortfolio2/"
+    },
+    {
+        title: "fitnessTracker",
+        url: "https://lit-gorge-03085.herokuapp.com/"
+    },
+    {
+        title: "noteTaker",
+        url: "https://fathomless-scrubland-82808.herokuapp.com/"
+    },
+    {
+        title: "userDirectory",
+        url: "https://andypedonti.github.io/goodEmployeeDir/"
+    },
 ];
