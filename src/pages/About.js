c6485a3494d1b62e1ddaeb600140a48e06e0d003
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-16 10:31:48 -0400

    rows in the card work

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,12 +7,23 @@ function About() {
     return (
         <Card>
             <div class="card-body">
-                <img src="./components/pics/018.jpg" height="225" width="150" >
+
+                <div className="item-a">
 
 
-                </img>
-                <h4 class="card-title">About Me</h4>
-                <h6 class="card-subtitle mb-2 text-muted">Husband, father, angler</h6>
+                    <img src="./components/pics/018.jpg" height="225" width="150" >
+
+
+                    </img>
+                    <h4 class="card-title">About Me</h4>
+                </div>
+
+                <div className="item-b">
+                    <h6 className="card-subtitle mb-2 text-muted">Husband, father, angler</h6>
+                </div>
+                <div className="item-c">
+                    <p>this hospital sucks</p>
+                </div>
 
             </div>
         </Card>
