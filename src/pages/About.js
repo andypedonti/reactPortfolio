ec25105446d364a9950c6dbd20212686480856e5
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-18 22:16:48 -0400

    grammer changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,9 +19,9 @@ function About() {
                 </div>
 
                 <div className="item-b">
-                    <p className="card-subtitle mb-2 text-muted">I am an leader and team builder with 14 years experience in the landscape industry. I enrich people lives
-                    by teaching the skills and ethics needed to advance in thier career. I hope to continue this leadership and enrichment through coding and web developement
-                    to enhance the life and prosperity of others weather it be through teamwork or thoughful content delivered through user friendly interfaces. </p>
+                    <p className="card-subtitle mb-2 text-muted">I am a leader and team builder with 14 years experience in the landscape industry. I enrich people's lives
+                    by teaching the skills and ethics needed to advance in their career. I hope to continue this leadership and enrichment through coding and web development
+                    to enhance the life and prosperity of others whether it be through teamwork or thoughtful content delivered through user friendly interfaces. </p>
                 </div>
 
 
