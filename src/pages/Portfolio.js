c2401b8b9fe61ce02fd092e3790e4ce10361f8ee
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2021-01-27 21:28:14 -0500

    links open in new tab

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,11 +12,11 @@ function Portfolio() {
                     portfolioContent.map((item) =>
 
                         <div className="portfolio-item">
-                            <a style={{ color: '#2F4F4F' }} href={item.url}>
+                            <a style={{ color: '#2F4F4F' }} href={item.url} target="_blank" rel="noopener noreferrer">
                                 <h3>{item.title}</h3>
                             </a>
                             <p>{item.description}</p>
-                            <a style={{ color: '#2F4F4F' }} href={item.github}>
+                            <a style={{ color: '#2F4F4F' }} href={item.github} target="_blank" rel="noopener noreferrer">
                                 <p>GitHub Repository</p>
                                 <img src={item.image}></img>
                             </a>
